1fe0b27d8f88dd7b23711bd334c58d8bd551584f
Sofia Latina <sofiaisabellatina@gmail.com>
2025-10-16 18:43:56 +0800

    updated vite

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/personal-portfolio/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
-  }
+ 
 })
